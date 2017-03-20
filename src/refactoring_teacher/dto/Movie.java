89de87330b2ede64473e8f0b1229672c0ce0f04b
net94 <net94@naver.com>
2017-03-20 10:22:56 +0900

    Movie Class 정의 완료

---
+++ src/refactoring_teacher/dto/Movie.java
@@ -0,0 +1,24 @@
+package refactoring_teacher.dto;
+
+public class Movie {
+	private String title;
+	private int priceCode;
+
+	public Movie(String title, int priceCode) {
+		this.title = title;
+		this.priceCode = priceCode;
+	}
+
+	public int getPriceCode() {
+		return priceCode;
+	}
+
+	public void setPriceCode(int priceCode) {
+		this.priceCode = priceCode;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+}
