938ff63160a1c1277458e818fcec258e4914a65a
net94 <net94@naver.com>
2017-03-20 12:20:09 +0900

    메서드 추출기법 적용완료

--- src/refactoring_teacher/dto/Customer.java
+++ src/refactoring_teacher/dto/Customer.java
@@ -28,30 +28,8 @@ public class Customer {
 		sb.append(getName() + "고객님의 대여기록\n");
 		
 		for(Rental each : rentals){
-			double thisAmount = 0;//비디오물당 대여료 
-									// 1. 일반물 (2일) 2000원, 일일 초과 1500, 적립 1
-									// 2. 아동물 (3일) 1500원, 일일 초과 1500, 적립 1
-									// 3. 최신물 (1일) 3000원, 일일 초과 3000, 적립 1 + 1
-			Movie movie = each.getMovie();
-			int priceCode = movie.getPriceCode();
-			int daysRented = each.getDaysRented();//해당 비디오물의 대여기간
-			switch(priceCode){
-			case Movie.REGULAR:
-				thisAmount = 2000;
-				if (daysRented > 2){
-					thisAmount += (daysRented-2) * 1500;
-				}
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount = daysRented * 3000;
-				break;
-			case Movie.CHILDREN:
-				thisAmount = 1500;
-				if (daysRented > 3){
-					thisAmount += (daysRented-3) * 1500;
-				}
-				break;
-			}
+			double thisAmount = amountFor(each);
+			
 			frequentRenterPoints++;
 			if (each.getMovie().getPriceCode()==Movie.NEW_RELEASE && each.getDaysRented() > 1){
 				frequentRenterPoints++;
@@ -65,5 +43,34 @@ public class Customer {
 		sb.append(String.format("누적 대여료 : %s%n적립 포인트 : %s%n", totalAmount, frequentRenterPoints));
 		
 		return sb.toString();
+	}
+
+	private double amountFor(Rental aRental) {
+		//비디오물당 대여료 
+		// 1. 일반물 (2일) 2000원, 일일 초과 1500, 적립 1
+		// 2. 아동물 (3일) 1500원, 일일 초과 1500, 적립 1
+		// 3. 최신물 (1일) 3000원, 일일 초과 3000, 적립 1 + 1
+		double result = 0;
+		Movie movie = aRental.getMovie();
+		int priceCode = movie.getPriceCode();
+		int daysRented = aRental.getDaysRented();//해당 비디오물의 대여기간
+		switch(priceCode){
+		case Movie.REGULAR:
+			result = 2000;
+			if (daysRented > 2){
+				result += (daysRented-2) * 1500;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			result = daysRented * 3000;
+			break;
+		case Movie.CHILDREN:
+			result = 1500;
+			if (daysRented > 3){
+				result += (daysRented-3) * 1500;
+			}
+			break;
+		}
+		return result;
 	}
 }
