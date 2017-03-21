bd6a6ba836e6209ee65cc16669517f2f2c30b823
net94 <net94@naver.com>
2017-03-21 11:09:27 +0900

    getCharge()->Movie로 이동

--- src/refactoring_teacher/dto/Movie.java
+++ src/refactoring_teacher/dto/Movie.java
@@ -26,4 +26,29 @@ public class Movie {
 		return title;
 	}
 
+	public double getCharge(int daysRented) {
+		//비디오물당 대여료 
+		// 1. 일반물 (2일) 2000원, 일일 초과 1500, 적립 1
+		// 2. 아동물 (3일) 1500원, 일일 초과 1500, 적립 1
+		// 3. 최신물 (1일) 3000원, 일일 초과 3000, 적립 1 + 1
+		double result = 0;
+		switch(getPriceCode()){
+		case Movie.REGULAR:
+			result = 2000;
+			if (daysRented > 2){
+				result += (daysRented-2) * 1500;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			result = daysRented * 3000;
+			break;
+		case Movie.CHILDREN:
+			result = 1500;
+			if (daysRented > 3){
+				result += (daysRented-3) * 1500;
+			}
+			break;
+		}
+		return result;
+	}
 }
